0baeb135ab3c01dad2d966d28c547d254e75eddb
VladMakarenko <vladyslav.makarenko.08@gmail.com>
2022-10-23 17:59:38 +0300

    Second commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main(request):
+    return HttpResponse("Hey! It's your main view!!")
+
+
+def articles(request):
+    return HttpResponse("articles")
+
+
+def archive(request):
+    return HttpResponse("<h1>archive</h1>")
+
+
+def users(request, user_number=''):
+    if user_number:
+        return HttpResponse(f'user_number={user_number}')
+    else:
+        return HttpResponse('users')
+
+
+def article(request, article_number='', slug_text=''):
+    if slug_text:
+        return HttpResponse(f'{article_number} + slug_text= {slug_text}')
+    elif article_number:
+        return HttpResponse(f'article_number = {article_number}')
+    else:
+        return HttpResponse(article_number)
+
+
+def regular(request):
+    return HttpResponse('regular')
